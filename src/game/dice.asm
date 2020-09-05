13916437bdb8fa7fa41bf6f54b0090953383eb7f
Daniel G <danielg.dev@gmail.com>
2020-09-05 12:36:53 +0100

    Add score calculation functions
    Add a function to load into register a the score for a given
    combination. Some take parameters specifying which type the score is
    for, others don't. Also correct some of the documentation on
    DICE_TYPE_SUM and _COUNT as the ordering was wrong. Some of these
    functions can probably be combined in future or streamlined a little
    (for instance, Chance can probably be reduced to a macro and OfAKind and
    Yatzy combined somehow).

--- src/game/dice.asm
+++ src/game/dice.asm
@@ -2,13 +2,13 @@ INCLUDE "hardware.inc"
 
 SECTION "Dice Variables", WRAM0
 
-DICE_SUM:: DS 1				; 1 byte with the sum of all the dice
-DICE_TYPE:: DS 1			; bit-array of dice types present
+DICE_SUM: DS 1				; 1 byte with the sum of all the dice
+DICE_TYPE: DS 1				; bit-array of dice types present
 					; 0 = not present, %--654321
-DICE_TYPE_SUM:: DS 6			; bytes holding the sum of all dice of
-					; each type (6, 5, 4, 3, 2, 1)
-DICE_TYPE_COUNT:: DS 6			; 6 bytes holding the number of dice of
-					; each type (6, 5, 4, 3, 2, 1)
+DICE_TYPE_SUM: DS 6			; bytes holding the sum of all dice of
+					; each type (1, 2, 3, 4, 5, 6)
+DICE_TYPE_COUNT: DS 6			; 6 bytes holding the number of dice of
+					; each type (1, 2, 3, 4, 5, 6)
 
 SECTION "Dice", ROM0
 
@@ -106,5 +106,159 @@ UpdateDice:
 	add hl, de
 	inc [hl]
 
+	ret
+
+; Calculate the score for a straight
+; @param b required DICE_TYPE bit pattern
+; @return a score
+Straight::
+	; able to make a straight comparision as only 5 dice
+	ld a, [DICE_TYPE]
+	cp b
+	jr nz, .fail
+	ld a, [DICE_SUM]		; for straights, score is sum of dice
+	ret
+.fail
+	ld a, 0
+	ret
+
+; Calculate score for a single number
+; @param a the dice number to calculate the score for
+; @return a the score
+; @return b 0
+; @return c dice number
+; @return hl DICE_TYPE_SUM + bc
+Single::
+	ld hl, DICE_TYPE_SUM
+	ld b, $00
+	ld c, a
+	dec bc				; make sure to decrement bc as
+					; DICE_TYPE_SUM is zero-indexed
+	add hl, bc
+	ld a, [hl]
+	ret
+
+; Calculate yatzy score
+; @return a score
+Yatzy::
+	; logical right shift til a 1 is found
+	ld a, [DICE_TYPE]
+.shiftStart
+	bit 0, a
+	jr nz, .shiftEnd
+	srl a
+	jr .shiftStart
+.shiftEnd
+	; perform a final right shift then compare a with 0
+	; if it matches, all 5 dice are the same number
+	srl a
+	jr nz, .noYatzy
+	ld a, 50			; score for a yatzy is 50
+	ret
+.noYatzy
+	ld a, 0
+	ret
+
+; Calculate a chance score. Just place [DICE_SUM] in a
+; @return a score
+Chance::
+	ld a, [DICE_SUM]
+	ret
+
+; Calculate score for pair of pairs
+TwoPairs::
+	ld b, 0				; keep track of distinct doubles
+	ld hl, DICE_TYPE_COUNT + 5	; loop over 6 dice from last one
+	ld d, 6				; keep track of current dice
+	ld c, 0				; keep track of score
+
+.loop
+	ld a, [hld]
+	cp 2
+	jr c, .notDouble		; if a < 2, not a double
+	inc b
+	ld a, c				; add 2*(current dice) to score
+	add d
+	add d
+	ld c, a
+.notDouble
+	dec d
+	jr nz, .loop
+
+	ld a, b				; check for prescence of two doubles
+	cp 2
+	jr c, .noScore			; if b < 2, score is 0
+	ld a, c				; else c holds final score
+	ret
+.noScore
+	ld a, 0
+	ret
+
+; Calculate full house score
+FullHouse::
+	ld b, 0				; keeps track of doubles
+	ld c, 0				; keeps track of triples
+
+	ld hl, DICE_TYPE_COUNT + 5	; loop from end of DICE_TYPE_COUNT array
+	ld d, 6				; loop over all 6 dice values,
+					; incrementing b and c as needed
+
+.loop
+	ld a, [hld]
+	cp 2
+	jr nz, .notDouble
+	inc b
+.notDouble
+	cp 3
+	jr nz, .notTriple
+	inc c
+.notTriple
+	dec d
+	jr nz, .loop
+
+	; for a full house, there must be a single double and single triple
+	; by and-ing both b and c, we'll get 1 if and only if this is the case
+	ld a, b
+	and c
+	cp %00000001
+	jr nz, .noFullHouse
+	ld a, [DICE_SUM]		; if full house present, score is sum
+					; of all dice
+	ret
+.noFullHouse
+	ld a, 0				; score zero otherwise
+	ret
+
+; Calculate the score for "Of A Kind" scores
+; @param b number of dice required to score
+; @return a score
+OfAKind::
+	ld hl, DICE_TYPE_COUNT + 5	; start at end to get highest score
+	ld d, 6				; repeat max 6 times for dice values
+
+	; find first dice value with a count greater than b
+.countLoop
+	ld a, [hld]
+	cp b
+	jr nc, .countEnd		; if a >= b break
+	dec d
+	jr nz, .countLoop		; if d > 0 return
+	ld a, 0
+	ret
+.countEnd
+
+	; subtract DICE_TYPE_COUNT from hl to get offset which = 6 - dice value
+	ld de, $FFFF - DICE_TYPE_COUNT - 1
+	add hl, de			; l now contains 6 - dice value
+	; subtract from six to get actual dice value
+	ld a, 6
+	sub l
+	ld l, a
+
+	ld a, 0
+.scoreLoop
+	add l
+	dec b
+	jr nz, .scoreLoop
 	ret
 
